cf60dabc16cb86ba6c47f2ef852ea77f580de140
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-02-10 11:13:34 +0000

    Quicksort

--- B_Vettori/SortingArray.cpp
+++ B_Vettori/SortingArray.cpp
@@ -10,14 +10,46 @@
 using namespace std;
 
 const string FILENAME = "SortingArray.txt";
+void swap(string arr[] , int pos1, int pos2){
+	string temp;
+	temp = arr[pos1];
+	arr[pos1] = arr[pos2];
+	arr[pos2] = temp;
+}
 
-void stampaVettore( string v[], int l ) {
+int partition(string arr[], int low, int high, string pivot){
+	int i = low;
+	int j = low;
+	while( i <= high){
+		if(arr[i] > pivot){
+			i++;
+		}
+		else{
+			swap(arr,i,j);
+			i++;
+			j++;
+		}
+	}
+	return j-1;
+}
+
+void quickSort(string arr[], int low, int high){
+	if(low < high){
+	string pivot = arr[high];
+	int pos = partition(arr, low, high, pivot);
+	
+	quickSort(arr, low, pos-1);
+	quickSort(arr, pos+1, high);
+	}
+}
+
+void stampaVettore(string v[], int l ) {
     cout << endl;
     for (int i=0; i<l; i++) cout << v[i] << "\n";
     cout << endl;
 }
 
-int bubbleSort( string v[], int l ) {
+int bubbleSort(string v[], int l ) {
     int numOp = 0;
     string tmp;
     for (int i=0; i<l; i++ ) {
@@ -36,8 +68,7 @@ int bubbleSort( string v[], int l ) {
     return numOp;
 }
 
-int main()
-{
+int main(){
     ifstream f;
     f.open(FILENAME);
     if ( f.fail() ) { cout << "Il file di input non esiste"; return -1; }
@@ -55,9 +86,12 @@ int main()
     for (int i=0; i<n; i++) getline( f, vs[i] );
 
     stampaVettore(vs, n);
-    int x = bubbleSort(vs, n);
-    cout << "Eseguiti " << x << " confronti." << endl;
+    //int x = bubbleSort(vs, n);
+    quickSort(vs, 0, n-1);
+    cout << "Eseguiti " << "?" << " confronti." << endl;
     stampaVettore(vs, n);
+
+    
 
     return 0;
 }
