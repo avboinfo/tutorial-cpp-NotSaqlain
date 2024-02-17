3add86efbd948f06199c305f75dcb62650042a7c
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-02-17 10:40:52 +0000

    Final update

--- F_Classi/Tris.cpp
+++ F_Classi/Tris.cpp
@@ -24,18 +24,42 @@ public:
     void stampa_griglia(){
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                cout << griglia[i][j];
+                cout << griglia[i][j] << "\t";
             }
             cout << endl;
         }
     }
 
     bool giocatore_uno(int x, int y){
+        if (x < 0 || x > 2){
+            return false;
+        }
 
+        if (y < 0 || y > 2){
+            return false;
+        }
+
+        if (griglia[x][y] == 1 || griglia[x][y] == 2){
+            return false;
+        }
+        griglia[x][y] = 1;
+        return true;
     }
 
     bool giocatore_due(int x, int y){
-        
+        if (x < 0 || x > 2){
+            return false;
+        }
+
+        if (y < 0 || y > 2){
+            return false;
+        }
+
+        if (griglia[x][y] == 1 || griglia[x][y] == 2){
+            return false;
+        }
+        griglia[x][y] = 2;
+        return true;
     }
 
 };
@@ -47,6 +71,33 @@ int main(int argc, char const* argv[]){
     myTris.reset_tabella();
 
     cout << "Griglia iniziale" << endl;
+    myTris.stampa_griglia();
+
+    int x, y;
+    bool mossa_valida;
+
+    do {
+        cout << "Giocatore 1: Inserisci le coordinate della tua mossa" << endl;
+        cout << "Inserisci la coordinata x: ";
+        cin >> x;
+        cout << "Inserisci la coordinata y: ";
+        cin >> y;
+        mossa_valida = myTris.giocatore_uno(x, y);
+    } while (!mossa_valida);
+
+    cout << "Griglia dopo la mossa" << endl;
+    myTris.stampa_griglia();
+
+    do {
+        cout << "Giocatore 2: Inserisci le coordinate della tua mossa" << endl;
+        cout << "X: ";
+        cin >> x;
+        cout << "Y: ";
+        cin >> y;
+        mossa_valida = myTris.giocatore_due(x, y);
+    } while (!mossa_valida);
+
+    cout << "Griglia dopo la mossa" << endl;
     myTris.stampa_griglia();
 
     return 0;
