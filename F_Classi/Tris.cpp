4439a698da210daf3587f9767d5a6d93d05d2ae9
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-02-17 09:47:35 +0000

    update

--- F_Classi/Tris.cpp
+++ F_Classi/Tris.cpp
@@ -1,9 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 /* 
 se in griglia c'e' ZERO cella vuota
 se in griglia c'e' UNO stiamo parlando di una X o (giocatore 1)
 se in griglia c'e' DUE stiamo parlando di un CERCHIO (giocatore 2)
 */
-
 
 class Tris{
 public:
@@ -15,6 +19,23 @@ public:
                 griglia[i][j]=0;
             }
         }
+    }
+
+    void stampa_griglia(){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                cout << griglia[i][j];
+            }
+            cout << endl;
+        }
+    }
+
+    bool giocatore_uno(int x, int y){
+
+    }
+
+    bool giocatore_due(int x, int y){
+        
     }
 
 };
