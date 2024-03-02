62892e450437d346d1807eaf8cc58bc4be9e27ef
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-03-02 09:29:03 +0000

    update

--- X_MasterMind/mastermind.cpp
+++ X_MasterMind/mastermind.cpp
@@ -4,12 +4,23 @@
 using namespace std;
 
 class mastermind {
+    int cella[4];
+    int ball = 0;   // Posizione sbagliata;
+    int strike = 0; // Posizione giusta
 
-
+    mastermind(int a, int b, int c, int d) {
+        cella[0] = a;
+        cella[1] = b;
+        cella[2] = c;
+        cella[3] = d;
+    }
 
 };
 
 int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
 
+    mastermind m(a, b, c, d);
     return 0;
 }
