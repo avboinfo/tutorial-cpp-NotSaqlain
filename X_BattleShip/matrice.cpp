66d3d8d3763aa7524f1e0788d157ce11f67b60e5
NotSaqlain <117275944+NotSaqlain@users.noreply.github.com>
2024-04-15 07:01:29 +0000

    add file

--- X_BattleShip/matrice.cpp
+++ X_BattleShip/matrice.cpp
@@ -10,20 +10,25 @@
 using namespace std;
 
 class matrice {
-private:
+private:    
     int m[10][10];
 
 public:
+    matrice() {
+        for (int i = 0; i < 10; i++)
+            for (int j = 0; j < 10; j++)
+                m[i][j] = rand() % 10;
+    }
     matrice(int n) {
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
-                m[i][j] = 0;
+                m[i][j] = n;
     }
 
-    void stampa(int n) {
+    void stampa() {
         cout << "----- Matrice -----\n";
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++)
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++)
                 cout << m[i][j] << " ";
             cout << endl;
         }
@@ -32,7 +37,10 @@ public:
 };
 
 int main() {
+    srand(time(NULL));
     matrice m(10);
-    m.stampa(10);
+    matrice n;
+    n.stampa();
+    m.stampa();
     return 0;
 }
