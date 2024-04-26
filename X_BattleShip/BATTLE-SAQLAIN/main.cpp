80203f2280558ef46f8be048ebd8922b0f1aaa63
NotSaqlain <iamnotsaqlain@gmail.com>
2024-04-26 22:49:32 +0200

    include playAgain function

--- X_BattleShip/BATTLE-SAQLAIN/main.cpp
+++ X_BattleShip/BATTLE-SAQLAIN/main.cpp
@@ -3,12 +3,22 @@
 
 using namespace std;
 
+bool playAgain() {
+    char c;
+    cout << "Vuoi giocare ancora? (s/n) ";
+    cin >> c;
+    return c == 's';
+}
+
 int main() {
     srand(time(NULL));
     //cout << "Hai 10 tentativi per colpire tutte le navi nemiche!" << endl;
-    cout << "------- BATTLESHIP GAME -------" << endl;
-    BattleShip gioco = BattleShip();
-    gioco.start();
-    cout << "GAME OVER!" << endl;
+    
+    do {
+        cout << "------- BATTLESHIP GAME -------" << endl;
+        BattleShip game;
+        game.start();
+        cout << "GAME OVER!" << endl;
+    } while (playAgain());
 
 }
